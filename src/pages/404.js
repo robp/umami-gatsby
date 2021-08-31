8cbc03f3c055a59ae0b1a80a2d98b9da2a84c34c
Rob Pinciuc <rob@pinciuc.com>
2021-08-30 19:21:25 -0700

    Remove breadcrumbs from 404 due to pageContext missing

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,27 +2,23 @@ import * as React from "react"
 import { graphql } from "gatsby"
 import { useTranslation } from "gatsby-plugin-react-i18next"
 
-import PageContextProvider from "../components/context/page-context"
-
 import Layout from "../components/layout/layout-default"
 import Seo from "../components/seo"
 
 import { container } from "../styles/layout.module.scss"
 
-const NotFoundPage = ({ pageContext }) => {
+const NotFoundPage = () => {
   const { t } = useTranslation()
 
   return (
-    <PageContextProvider pageContext={pageContext}>
-      <Layout title={`404: ${t("Not found")}`}>
-        <Seo title={`404: ${t("Not found")}`} />
-        <div className={container}>
-          <p>
-            {t("You just hit a route that doesn&#39;t exist... the sadness.")}
-          </p>
-        </div>
-      </Layout>
-    </PageContextProvider>
+    <Layout title={`404: ${t("Not found")}`}>
+      <Seo title={`404: ${t("Not found")}`} />
+      <div className={container}>
+        <p>
+          {t("You just hit a route that doesn&#39;t exist... the sadness.")}
+        </p>
+      </div>
+    </Layout>
   )
 }
 
